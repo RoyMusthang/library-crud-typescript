efab97b65b2c414ae0d7eac7869cc1f4308a8685
Josué Lobo <darksiders20005@gmail.com>
2022-02-21 15:26:49 -0300

    refactor: função readfile implementada

---
+++ utils.ts
@@ -0,0 +1,14 @@
+import fs from 'fs/promises';
+import Book from './interfaces/Book';
+
+async function read(): Promise<Book[]> {
+  const data = await fs.readFile("./books.json", "utf8");
+
+  const books: Book[] = JSON.parse(data);
+
+  return books;
+}
+
+export {
+  read,
+}
