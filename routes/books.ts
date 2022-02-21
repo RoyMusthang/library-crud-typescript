be91157d072ae3b27254c8b0ca2f1228219c033b
Josué Lobo <darksiders20005@gmail.com>
2022-02-21 15:07:39 -0300

    feat: adicionado script de dev, é getAll implementado

--- routes/books.ts
+++ routes/books.ts
@@ -1,9 +1,13 @@
 import { Router, Request, Response } from 'express';
+import fs from 'fs/promises';
+import Book from '../interfaces/Book';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-
+router.get('/', async (req: Request, res: Response) => {
+  const data = await fs.readFile('./books.json', 'utf8');
+  const books: Book[] = await JSON.parse(data);
+  return res.status(200).json(books);
 });
 
 router.get('/:isbn', (req: Request, res: Response) => {
