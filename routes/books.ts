17f2dca2e81cb92fbc5a503a952b5935510d0923
Josué Lobo <darksiders20005@gmail.com>
2022-02-21 15:54:49 -0300

    feat: create route book of type put

--- routes/books.ts
+++ routes/books.ts
@@ -34,8 +34,20 @@ router.post('/', validationBook, async (req: Request, res: Response) => {
   res.status(StatusCode.CREATED).json(book);
 });
 
-router.put('/', (req: Request, res: Response) => {
+router.put('/', validationBook, async (req: Request, res: Response) => {
+  const { isbn } = req.params;
 
+  const editedBook: Book = req.body;
+  const books = await read();
+
+  const index = books.findIndex(book => book.isbn === isbn);
+
+  if (index === -1) return res.status(StatusCode.NOT_FOUND).send(NotFoundMessage);
+
+  books.splice(index, 1, editedBook);
+  await write(books);
+
+  return res.status(StatusCode.OK).json(editedBook);
 })
 
 router.delete('/:isbn', (req: Request, res: Response) => {
